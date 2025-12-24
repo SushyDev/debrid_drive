4b880172b20e227478f97ff0d81ce771bdda93bd
SushyDev <mail@sushy.dev>
2025-12-24 14:47:32 +0100

    on-demand link

--- apps/grpc_server/lib/grpc_server/file_system_service/server.ex
+++ apps/grpc_server/lib/grpc_server/file_system_service/server.ex
@@ -522,7 +522,10 @@ defmodule GrpcServer.FileSystemService.Server do
   end
 
   defp is_streamable?(inode) do
-    VFS.Streamability.streamable?(inode)
+    # All virtual inodes should be marked as streamable, even if they don't have a link yet
+    # This ensures the FUSE client uses StreamableNode instead of FileNode
+    # If there's no link available, get_stream_url will return an error gracefully
+    VFS.is_virtual_inode?(inode)
   end
 
   # Resolves an inode for file operations (read, write, etc.)
